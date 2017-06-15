404cdc8580654d0cdb33eceff457c743f17f8d27
rajsimran1996@gmail.com <Your Email Id>
2017-06-15 12:54:24 +0530

    Adding the AccessToken and main file

---
+++ main.py
@@ -0,0 +1,12 @@
+# Importing the APP_ACCESS_TOKEN
+from access_token.py import APP_ACCESS_TOKEN
+
+# Importing Requests library to make network requests
+import requests
+
+# Importing termcolor and colorama to get a colorful output
+from colorama import init
+from termcolor import colored
+
+# To initialize the colorama
+init()
