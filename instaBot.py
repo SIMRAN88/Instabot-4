72eeaf8f50de7cc121e218a92efd21420bf4d927
rajsimran1996@gmail.com <Your Email Id>
2017-06-18 20:35:04 +0530

    Get the recent posts of an user along with caption

--- instaBot.py
+++ instaBot.py
@@ -3,6 +3,8 @@ from access_token import APP_ACCESS_TOKEN
 
 # Importing Requests library to make network requests
 import requests
+# Importing urlib to download the posts
+import urllib
 
 # Importing termcolor and colorama to get a colorful output
 from colorama import init
@@ -14,9 +16,9 @@ from termcolor import colored
 # Base URL common for all the requests in the file.
 BASE_URL = 'https://api.instagram.com/v1/'
 
-# **************************************
+# *****************************************
 # Function declaration to get your own info
-# **************************************
+# *****************************************
 
 def self_info():
     request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
@@ -99,6 +101,70 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+# **************************************************
+# Function declaration to get recent post of yourself
+# **************************************************
+
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            if own_media['data'][0]['caption']['text'] != '':
+                print(colored("Caption:",'yellow')),
+                print (colored(own_media['data'][0]['caption']['text'],'red'))
+                print(colored("Image Name:", "blue")),
+                print image_name
+            else:
+                print(colored("Image Name","blue")),
+                print image_name
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+# ****************************************************************
+# Function declaration to get the recent post of a user by username
+# ****************************************************************
+
+
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            if user_media['data'][0]['caption']['text'] != '':
+                print(colored("Caption:",'yellow')),
+                print (colored(user_media['data'][0]['caption']['text'],'red'))
+                print(colored("Image Name:", "blue")),
+                print image_name
+            else:
+                print(colored("Image Name", "blue")),
+                print image_name
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+
+
+
 def start_bot():
     while True:
         print (colored(('-' *100), 'red'))
@@ -110,7 +176,9 @@ def start_bot():
         print 'Here are your menu options:'
         print "a.Get your own details"
         print "b.Get details of a user by username"
-        print "c.Exit"
+        print "c.Get your own recent post"
+        print "d.Get the recent post of a user by username"
+        print "e.Exit"
 
         choice=raw_input("Enter you choice: ")
         if choice=="a":
@@ -118,7 +186,12 @@ def start_bot():
         elif choice=="b":
             insta_username = raw_input("Enter the username of the user: ")
             get_user_info(insta_username)
-        elif choice=="c":
+        elif choice == "c":
+            get_own_post()
+        elif choice == "d":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_post(insta_username)
+        elif choice == "e":
             exit()
         else:
             print (colored("You chose an invisible choice.Try Again",'magenta'))
@@ -126,3 +199,4 @@ def start_bot():
 
 
 start_bot()
+get_own_post()
