168c70ce661761fc2e1a1dce5a3ee4df79bfbbef
rajsimran1996@gmail.com <Your Email Id>
2017-06-21 17:57:38 +0530

    Search for a word in a comment,function added

--- instaBot.py
+++ instaBot.py
@@ -279,6 +279,40 @@ def post_a_comment(insta_username, option, post_selection):
     else:
         print(colored('Your comment was unsuccessful. Try again!',"red"))
 
+# ***************************************************************************************
+#  The Function gives Id of a comment that contains a particular word in a particular post
+# ***************************************************************************************
+def word_search_in_comment(username, option, post_selection):
+    media_id = search_post_by_choice(username, option,
+                                     post_selection)  # search_post_by_choice(username) function called here to get post ID
+    url_post_comment = BASE_URL + "media/" + media_id + "/comments?access_token=" + APP_ACCESS_TOKEN
+    all_comments = requests.get(url_post_comment).json()
+    search_word = raw_input("Enter a word you want to search in the comments")
+    print "\n<>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<>"
+    comments_id = []
+    list_of_comments = []
+    user_name = []
+    for each_comment in all_comments['data']:
+        list_of_comments.append(each_comment['text'])
+        comments_id.append(each_comment['id'])
+        user_name.append(each_comment['from']['username'])
+    comments_id_matched = []
+    comments_matched = []
+    user_found = []
+    for each_item in range(len(list_of_comments)):  # Search for the comment that contains the specified word
+        if search_word in list_of_comments[each_item]:
+            comments_matched.append(list_of_comments[each_item])
+            comments_id_matched.append(comments_id[each_item])
+            user_found.append(user_name[each_item])
+    if len(comments_matched) == 0:  # No comment Found with the word you searched
+        print "No comment have word:" + search_word
+        return False, media_id, False
+    else:  # Comment found with word search!
+        print "These comments contains the word:" + search_word
+        for i in range(len(comments_matched)):
+            print(">>>>>>>" + comments_matched[i])
+        return comments_id_matched, media_id, comments_matched
+
 
 # ***************************************************************************
 # Function declaration to make delete negative comments from the recent post
