340808cc7860f00aec827c28ba7c5895d4e6355e
rajsimran1996@gmail.com <Your Email Id>
2017-06-21 17:48:02 +0530

    Fetch all public post function added

--- instaBot.py
+++ instaBot.py
@@ -204,6 +204,15 @@ def get_post_id_recent(insta_username):
     else:
         print 'Status code other than 200 received!'
         exit()
+        
+# *******************************************************************************************************
+# The function below fetches all public post starting from the most recent one published by the user using 'GET'.
+# **********************************************************************************************************
+def get_user_post(username):
+    user_id = get_user_id(username)  # get_user_id(username) function called here to get the user's ID
+    user_url = BASE_URL + "users/" + user_id + "/media/recent/?access_token=" + APP_ACCESS_TOKEN
+    request_user_recent_post = requests.get(user_url).json()  # GET call to fetch user's post
+    return request_user_recent_post
 
 # ***************************************************************************
 # Function declaration to make delete negative comments from the recent post
